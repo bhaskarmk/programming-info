363cb6d56d001f649197bdb7356c83ad90d96e53
bhaskarmk <bhaskarmkul1098@gmail.com>
2023-02-07 21:20:18 +0530

    loops10to1

---
+++ for2.sh
@@ -0,0 +1,6 @@
+echo "this is another for loop for printing the numbers from 1 to 100"
+echo "-------------------------------------------"
+for (( k=1;k<=100;k++ ))
+do
+	echo "$k"
+done
---
+++ for3.sh
@@ -0,0 +1,8 @@
+echo "hii"
+echo "good morning"
+echo "this is another for loops three prgram from 10 to 1"
+echo "--------------------------"
+for (( a=10;a>=1;a-- ))
+do 
+	echo "$a"
+done

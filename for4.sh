74995efb55cbfb7ae54f6a2607c1a465bf6c4299
bhaskarmk <bhaskarmkul1098@gmail.com>
2023-02-07 21:36:23 +0530

    my name n role

---
+++ for4.sh
@@ -0,0 +1,6 @@
+echo "this is another for loop for print my name and role name"
+echo "---------------------------------------"
+for (( i=1;i<=20;i++ ))
+do 
+	echo "bhaskar and my role is DevOps"
+done
---
+++ for5.sh
@@ -0,0 +1,6 @@
+echo "this is anther loop concept by me"
+echo "---------------------------"
+for (( i=0;i<=10;i++ ))
+do
+	echo "prabhas"
+done

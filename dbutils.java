86fe765dc1f25ed4aaaa98e2440d504c36c42ff3
bhaskarmk <bhaskarmkul1098@gmail.com>
2023-02-07 23:29:22 +0530

    java,db

---
+++ dbutils.java
@@ -0,0 +1,3 @@
+echo "hii"
+echo "this file contains db backup"
+echo "this file is conatins all the data backup"
---
+++ javautils.java
@@ -0,0 +1,5 @@
+echo "hiii"
+echo "hello"
+echo "hello"
+echo "this file may conatns java related info"
+
